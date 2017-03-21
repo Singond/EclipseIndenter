87a98fb8089589905385776ac8602370cb1e3faa
Jan Slany <jan.slany@ais-servis.cz>
2017-03-21 17:40:39 +0100

    Clean code

--- src/cz/slanyj/eclipse/indenter/TabWidthHandler.java
+++ src/cz/slanyj/eclipse/indenter/TabWidthHandler.java
@@ -30,29 +30,9 @@ public class TabWidthHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		String optString = event.getParameter("cz.slanyj.eclipse.indenter.params.tabWidth");
 		short tabWidth = Short.valueOf(optString);
-		
 		for (TabWidthSetter s : setters) {
 			s.setTabWidth(tabWidth);
 		}
-		
-		// Display the selected option
-		/*IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Setting Tab Width",
-				"Selected tab width is "+tabWidth);*/
-		
-		// Works for the general setting, but is overriden in Java editors
-		/*IPreferenceStore ps = new ScopedPreferenceStore(InstanceScope.INSTANCE,
-				"org.eclipse.ui.editors");
-		ps.setValue("tabWidth", (int)tabWidth);*/
-		
-		// Works with Java editors
-		/*IPreferenceStore ps = new ScopedPreferenceStore(InstanceScope.INSTANCE,
-				"org.eclipse.jdt.core");
-		ps.setValue("org.eclipse.jdt.core.formatter.tabulation.size", (int)tabWidth);
-		ps.setValue("org.eclipse.jdt.core.formatter.indentation.size", (int)tabWidth);*/
-		
 		return null;
 	}
 }
